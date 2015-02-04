bb52ff8a9e3a715cee1fc4acb8a6e1e7235c9c50
Alex Cioc <acioc@caltech.edu>
2015-02-04 02:17:40 -0800

    Removed TODO

--- src/edu/caltech/nanodb/qeval/SimplePlanner.java
+++ src/edu/caltech/nanodb/qeval/SimplePlanner.java
@@ -124,7 +124,6 @@ public class SimplePlanner implements Planner {
         if (whereExpr != null) {
             finalPlan = new SimpleFilterNode(finalPlan, whereExpr);
         }
-        // TODO: SEE IF WE NEED MORE WHERE CLAUSE STATEMENT RELATED THINGS
 
         // We deal with our order by expressions
         List<OrderByExpression> orderByExprs = selClause.getOrderByExprs();
